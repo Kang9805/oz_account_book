e06814d7e9c95d1fde2dcf927b603dd93eeb2a1a
kang9805 <zzionkk@naver.com>
2025-10-06 20:07:40 +0900

    feat: 이메일 기반 커스텀 유저 모델로 전환 및 마이그레이션 적용

--- users/models.py
+++ users/models.py
@@ -1,25 +1,31 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    """유저 모델 정의"""
+from .managers import UserManager
 
-    email = models.EmailField(unique=True, verbose_name='이메일')
-    nickname = models.CharField(max_length=50, unique=True, verbose_name='닉네임')
-    name = models.CharField(max_length=100, blank=True, verbose_name='이름')
-    phone_number = models.CharField(max_length=20, blank=True, verbose_name='휴대폰 번호')
 
-    # 이메일을 주 식별자로 설정
-    username = None
-    EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['nickname', 'name', 'phone_number']
+class User(AbstractBaseUser, PermissionsMixin):
+    """커스텀 유저 모델"""
 
+    email = models.EmailField(unique=True, verbose_name="이메일")
+    nickname = models.CharField(max_length=50, unique=True, verbose_name="닉네임")
+    name = models.CharField(max_length=100, blank=True, verbose_name="이름")
+    phone_number = models.CharField(
+        max_length=20, blank=True, verbose_name="휴대폰 번호"
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()  # 커스텀 매니저 연결
+
+    USERNAME_FIELD = "email"  # 이메일을 로그인 ID로 사용
+    REQUIRED_FIELDS = ["nickname", "name", "phone_number"]  # 슈퍼유저 생성 시 필수 입력
 
     class Meta:
-        db_table = 'users'
-        verbose_name = '사용자'
-        verbose_name_plural = '사용자 목록'
+        db_table = "users"
+        verbose_name = "사용자"
+        verbose_name_plural = "사용자 목록"
 
     def __str__(self):
         return self.email
